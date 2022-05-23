cff76ec54be638a79cc1f55d9da68eb40bc956db
BulyginK <bulygin.k@gmail.com>
2022-05-23 23:32:53 +0300

    +

--- js/calculator.js
+++ js/calculator.js
@@ -6,24 +6,36 @@ let btnAppend = document.querySelector('#append');
 
 let commands = ['var', 'let', 'fn', 'print', 'printvars', 'printfns'];
 
+
 class Vars {
-    constructor() {
-    }  
+    checkVar(nameVar) {
+        for (let key in vars) {
+            if (nameVar == key) {
+                alert('Такая переменная уже существует!')
+                return
+            }
+        }
+        vars[nameVar] = NaN;
+    }
+
+    addMeaning(nameVar, meaning) {
+        vars[nameVar] = +meaning;
+    }
 }
 
 btnAppend.addEventListener('click', () => {
     let nameOperation = command.value.split(' ')[0];
     let bodyOperation = command.value.split(' ')[1];
-
     let operation = commands.find(item => nameOperation === item);
+    let nameVar = bodyOperation.split('=')[0];
+    let meaning = bodyOperation.split('=')[1];
 
     if (operation === commands[0]) {
-        vars[bodyOperation] = NaN
+        vars.checkVar(bodyOperation)
     } else if (operation === commands[1]) {
-        let nameVar = bodyOperation.split('=')[0];
-        let meaning = bodyOperation.split('=')[1];
-        vars[nameVar] = +meaning
+        vars.addMeaning(nameVar, meaning);
     }
+
     console.log(vars);
 })
 
